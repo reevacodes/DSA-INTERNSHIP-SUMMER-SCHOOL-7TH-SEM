4ba45562567174927201a64c586e9168f3598171
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-02 02:18:16 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int>v={23,10,24,78,89,45};
+
+    cout<<v.front()<<endl;
+    cout<<v.back()<<endl;
+
+    cout << *v.begin()<<endl;  //return the reference of 0th index
+    cout << *(v.end()-1)<<endl;  //return the reference of just after the last element
+    return 0;
+}
