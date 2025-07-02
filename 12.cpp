a9f723a5dacd2b78a20b255c3cde5bd71f44bdc1
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-03 00:21:23 +0530

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,12 @@
+// CHANGING THE CONST VALUE
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    const int num = 20;
+    // int *ptr = & num;    ERROR IN NEW VERSION
+    // *ptr = 10;
+    // cout<<*ptr;          O/P-> 10(IN OLD VERSION)    
+    return 0;   
+}
