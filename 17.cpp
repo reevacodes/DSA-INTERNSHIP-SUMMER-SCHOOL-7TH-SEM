42c78da987241ea73a9c7125df6604d5bb432eff
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-03 00:23:10 +0530

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {10,20,30,40,50};
+
+    cout<<arr<<endl;  // address of 1st ele
+    cout<<arr[2]<<endl; //30
+
+    return 0;
+}
