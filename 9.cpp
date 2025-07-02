91e00d80f8b41374203c9e9b4b2e8e0e54c74498
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-03 00:20:22 +0530

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,33 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int num = 20;
+//     int *ptr = &num;
+//     int &x = num;
+
+//     cout<<num<<endl;
+//     cout<<ptr<<endl;
+//     cout<<x<<endl;
+
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int num=10;
+
+// int main(){
+//     int num = 20;
+//     int *ptr = &num;
+//     int &x = num;
+
+//     cout<<num<<endl;
+//     cout<<::num<<endl; //need to use scope resolution operator to print global value
+//     cout<<ptr<<endl;
+//     cout<<x<<endl;
+
+//     return 0;
+// }
+
