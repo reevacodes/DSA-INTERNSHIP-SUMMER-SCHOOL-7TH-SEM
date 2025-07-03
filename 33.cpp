b24442efa1b9e720bf80833b5516d7ecb111ad17
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-04 01:02:33 +0530

    Add files via upload

---
+++ 33.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void sum(int num){
+	
+	// static is a storage class which is makes variable as global. 
+	static int s=0;	
+	
+	if( num == 0 ){
+		
+		std::cout<< "sum is: "<<s;
+		return;
+	}
+		
+	s += num;
+	sum( num-1 );
+}
+
+int main(){
+	
+	sum( 5 );
+	
+//	cout<< "Sum is: "<< s;
+	
+	return 0;
+}
