c7921590c422d1f4174041084e38f0f88baad729
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-04 01:03:54 +0530

    Add files via upload

---
+++ 37.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int s, int e, int n){
+	
+	if( s>e )
+		return -1;
+		
+	int mid = (s+e) / 2;
+	
+	if( arr[mid] == n )
+		return mid;
+	
+	if( arr[mid] < n )
+		BinarySearch(arr, mid+1, e, n);
+	
+	else
+		BinarySearch(arr, s, mid-1, n);
+}
+
+int main(){
+	
+	int arr[5] = {1, 2, 3, 4, 5};
+	
+	int size = sizeof(arr)/sizeof(int);
+	
+	cout<<"sum: "<<BinarySearch(arr, 0, size-1, 4);
+	
+	return 0;
+}
