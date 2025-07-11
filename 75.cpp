9865121dad98674bba90da9aade29942ec28857c
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-12 00:23:17 +0530

    Add files via upload

---
+++ 75.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class node{
+	
+	public:
+		int data;
+		node* prev;
+		node* next;
+		
+		node(int d): data(d), prev(NULL), next(NULL){}
+};
+
+void print( node* head ){
+	
+	node* temp = head;
+	
+	while( temp->next != head ){
+		
+		cout<< temp->prev <<"\t"<< temp->data<<"\t"<<temp->next <<endl;
+		
+		temp = temp->next;
+	}
+	cout<< temp->prev <<"\t"<< temp->data<<"\t"<<temp->next <<endl;
+}
+
+int main(){
+	
+	node* head = new node(10);
+	head->prev = head;
+	head->next = head;
+	
+	node* n2 = new node(20);
+	n2->next = head;
+	n2->prev = head;
+	head->next = n2;
+	head->prev = n2;
+	
+	node* n3 = new node(30);
+	n3->next = head;
+	n3->prev = n2;
+	head->prev = n3;
+	n2->next = n3;
+	
+	print( head );
+	
+	return 0;
+}
